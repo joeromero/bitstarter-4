58084ee4f1a391005a292d983a339bae8923518e
app <app.app@gmail.com>
2013-07-16 00:55:52 +0200

    cambios, personalizacion index , hw04 parte 2

--- web.js
+++ web.js
@@ -7,7 +7,9 @@ app.get("/", function(request, response) {
   response.send(fs.readFileSync("index.html").toString());
 });
 
-var port = process.env.PORT || 5000;
+app.use("/imgs", express.static(__dirname + "/imgs"));
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
